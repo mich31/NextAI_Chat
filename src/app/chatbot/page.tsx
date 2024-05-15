0f49e6d7d601407f0e7e6621274b153ac119b2e2
mich31 <michelaxel@hotmail.com>
2024-05-15 21:23:01 +0200

    refactor message panel

--- src/app/chatbot/page.tsx
+++ src/app/chatbot/page.tsx
@@ -3,18 +3,37 @@
 import { PaperPlaneIcon } from '@radix-ui/react-icons';
 import Message from '../ui/message';
 
+type MessageContent = {
+    id: number,
+    content: string,
+    role: 'ai' | 'user',
+};
+
 export default function Page(){
-    const message = 'I have a very long message to send. This is very long so I need to adapt this component.';
+    const messages: MessageContent[] = [
+        {id: 1, content: 'Hi', role: 'user'},
+        {id: 2, content: 'Hi, how can I help you ?', role: 'ai'},
+        {id: 3, content: 'What are the benefits of fine tuning a llm model ?', role: 'user'},
+        {
+            id: 4, 
+            content: 'Fine-tuning a large language model (LLM) can offer several benefits: Task-Specific Performance Improvement, Customization, Resource Efficiency, Faster Deployment, Transfer Learning, Domain Adaptation',
+            role: 'ai'
+        },
+        {id: 5, content: 'Ok', role: 'user'},
+        {id: 6, content: 'Can I help you with something else ?', role: 'ai'},
+        {id: 7, content: 'I have a very long message to send. This is very long so I need to adapt this component.', role: 'user'},
+        {id: 8, content: 'Fine! Can you provide me more details about this message ?', role: 'ai'},
+        {id: 9, content: 'I would like to write an essay about the impact of generative AI on education', role: 'user'},
+        {id: 10, content: 'Great! That\'s a really interesting topic. I would be glad to help you on this but I need a few more details', role: 'ai'},
+    ]
     return (
         <div className='flex flex-row'>
-            <div className='min-h-screen basis-1/4 bg-blue-300 p-2'>Conversations</div>
-            <div className='flex min-h-screen basis-3/4 flex-col bg-white'>
-                <div className='mx-auto w-10/12 basis-11/12 bg-white px-2 py-2 rounded-xl mt-8 shadow-2xl'>
-                    <Message content={message} role='ai'/>
-                    <Message content='Hi, Hello, Good morning!' role='user'/>
-                    <Message content='Hey!' role='ai'/>
+            <div className='max-h-screen basis-1/4 bg-blue-300 p-2'>Conversations</div>
+            <div className='flex max-h-screen basis-3/4 overflow-hidden flex-col bg-white'>
+                <div className='mx-auto overflow-y-auto w-10/12 h-[90%] bg-white px-2 py-2 rounded-xl mt-8 shadow-2xl'>
+                    {messages.map((m) => (<Message key={m.id} content={m.content} role={m.role}/>))}
                 </div>
-                <div className='flex mx-auto w-10/12 basis-1/12 py-4 space-x-4'>
+                <div className='flex mx-auto w-10/12 py-4 space-x-4'>
                     <input 
                         type='text'
                         className='basis-11/12 rounded-lg border-2 border-blue-300 px-2 py-2 focus:border-blue-900'
