16cf5854e2caa9d8d6bc9139bc2dc2d110fd4687
mich31 <michelaxel@hotmail.com>
2024-05-16 15:08:51 +0200

    implement error handling on server action

--- src/app/api/chat/v2/route.ts
+++ src/app/api/chat/v2/route.ts
@@ -5,11 +5,17 @@ export async function POST(req: Request) {
   // const { prompt }: { prompt: string } = await req.json();
   const { messages } = await req.json();
 
-  const result = await streamText({
-    model: openai('gpt-3.5-turbo'),
-    system: 'You are a helpful assistant. Do not reveal your model version.',
-    messages,
-  });
-
-  return result.toAIStreamResponse();
+  try {
+    const result = await streamText({
+      model: openai('gpt-3.5-turbo'),
+      system: 'You are a helpful assistant. Do not reveal your model version.',
+      messages,
+    });
+  
+    return result.toAIStreamResponse(); 
+  } catch (error: any) {
+    // TODO: stream custom error message
+    console.error(error.message);
+    return { error: 'something went wrong', details: error.message };
+  }
 }
