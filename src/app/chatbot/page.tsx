c8e8e1230af770bf3b6e8010ad57d2a33dfeee23
mich31 <michelaxel@hotmail.com>
2024-05-17 13:30:32 +0200

    Enable analytics

--- src/app/chatbot/page.tsx
+++ src/app/chatbot/page.tsx
@@ -13,7 +13,7 @@ export default function Page(){
         <div className='flex flex-row'>
             <div className='h-screen basis-1/4 bg-blue-300 p-2'>Conversations</div>
             <div className='flex h-screen basis-3/4 overflow-hidden flex-col bg-white'>
-                <div className='mx-auto overflow-y-auto w-10/12 h-[90%] bg-white px-2 py-2 rounded-xl mt-8 shadow-2xl'>
+                <div className='mx-auto overflow-y-auto w-10/12 h-[90%] bg-white px-2 py-2 rounded-xl mt-8 shadow-xl'>
                     {messages.map((m) => (<Message message={m} key={m.id}/>))}
                 </div>
                 <form onSubmit={handleSubmit} className='flex mx-auto w-10/12 py-4 space-x-4'>
@@ -23,12 +23,12 @@ export default function Page(){
                         name='message'
                         value={input}
                         onChange={handleInputChange}
-                        className='basis-11/12 rounded-lg border-2 border-blue-300 px-2 py-2 focus:border-blue-900'
+                        className='basis-11/12 rounded-lg border-2 border-blue-300 px-2 py-2 focus:border-blue-900 shadow-2xl'
                         placeholder='Type message..' 
                     />
                     <button
                         type='submit'
-                        className='px-2 py-2 basis-1/12 bg-blue-300 rounded-xl w-64 hover:opacity-80'
+                        className='px-2 py-2 basis-1/12 bg-blue-300 rounded-xl w-64 hover:opacity-90 shadow-2xl'
                     >
                         <PaperPlaneIcon className='w-6 h-6 mx-auto text-white'/>
                     </button>
