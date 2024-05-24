5d9aa8e68fca0fbaef99df9de8eb27b947f4d1fb
mich31 <michelaxel@hotmail.com>
2024-05-24 19:53:08 +0200

    stop response generation

--- src/app/chatbot/v2/page.tsx
+++ src/app/chatbot/v2/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 
 import { Textarea } from '@/app/ui/textarea';
-import { PaperPlaneIcon } from '@radix-ui/react-icons';
+import { PaperPlaneIcon, StopIcon } from '@radix-ui/react-icons';
 import { useChat } from 'ai/react';
 import ChatMessage from '@/app/ui/message';
 import { useEnterSubmit } from '@/lib/hooks/use-enter-submit';
 
 export default function Page() {
-    const { messages, input, handleInputChange, handleSubmit, isLoading, setMessages, reload } = useChat({
+    const { messages, input, handleInputChange, handleSubmit, isLoading, stop, setMessages, reload } = useChat({
         api: '../api/chat/v2',
     });
     const { formRef, onKeyDown } = useEnterSubmit();
@@ -38,7 +38,7 @@ export default function Page() {
                 </div>
             </div>
             <div className='flex flex-col basis-[77%] overflow-y-auto bg-slate-50'>
-                <div className='mx-auto w-10/12  pt-4 h-full'>
+                <div className='mx-auto w-10/12 pt-4 h-full'>
                     {messages.map((m) => (<ChatMessage message={m} key={m.id}/>))}
                     <div className='h-[160px]'></div>
                 </div>
@@ -49,11 +49,10 @@ export default function Page() {
                                 <form ref={formRef} onSubmit={handleSubmit}>
                                     <div className='relative flex flex-row pr-20 border rounded-md'>
                                         <Textarea
-                                            // ref={inputRef}
                                             tabIndex={0}
                                             onKeyDown={onKeyDown}
                                             placeholder='Type a message..'
-                                            className='min-h-[60px] w-full resize-none bg-transparent px-4 py-[1.3rem] focus-within:outline-none sm:text-sm'
+                                            className='min-h-[60px] w-full resize-none bg-transparent px-4 py-[1.3rem] focus-within:outline-none sm:text-sm disabled:cursor-not-allowed'
                                             autoFocus
                                             spellCheck={false}
                                             autoComplete='off'
@@ -62,14 +61,18 @@ export default function Page() {
                                             rows={1}
                                             value={input}
                                             onChange={handleInputChange}
+                                            disabled={isLoading}
                                         />
                                         <div className='absolute right-0 top-3 sm:right-4'>
                                             <button
                                                 type='submit'
                                                 className='px-2 py-2 basis-1/12 bg-blue-500 rounded-xl w-12 hover:opacity-90 shadow-2xl disabled:cursor-not-allowed disabled:bg-blue-200'
-                                                disabled={input.trim() === ''}
+                                                disabled={input.trim() === '' && !isLoading}
                                             >
-                                                <PaperPlaneIcon className='w-6 h-6 mx-auto text-white'/>
+                                                {
+                                                    isLoading ? 
+                                                        <StopIcon onClick={stop} className='w-6 h-6 mx-auto text-white'/> : <PaperPlaneIcon className='w-6 h-6 mx-auto text-white'/>
+                                                }
                                             </button>
                                         </div>
                                     </div>
