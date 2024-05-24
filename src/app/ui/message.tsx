6bf2c26868f7aee1b05cf653d3b67c0382f87fcf
mich31 <michelaxel@hotmail.com>
2024-05-24 20:28:37 +0200

    add copy button

--- src/app/ui/message.tsx
+++ src/app/ui/message.tsx
@@ -6,57 +6,69 @@ import remarkMath from 'remark-math'
 import { MemoizedReactMarkdown } from './markdown';
 import Link from 'next/link';
 import { useState } from 'react';
+import { CopyIcon } from '@radix-ui/react-icons';
 
-export default function ChatMessage( {message} : {message: Message}) {
+export default function ChatMessage( {message, isLoading} : {message: Message, isLoading: boolean}) {
     const [url, setUrl] = useState<URL>();
-    const avatar = message.role === 'user' ? '' : 'https://github.com/shadcn.png';
+    const isUser: boolean = message.role === 'user';
+    const avatar = isUser ? '' : 'https://github.com/shadcn.png';
     return (
-        <div 
-            className={clsx(
-                'flex items-start p-2 max-w-full',
-                {
-                    'flex-row-reverse': message.role === 'user'
-                }
-            )}
-        >
-            <Avatar.Root className='h-8 w-8 my-1 select-none items-center justify-center overflow-hidden rounded-full align-middle'>
-                <Avatar.Image className='h-full w-full rounded-[inherit] object-cover' src={avatar} />
-                <Avatar.Fallback 
-                    className='text-white leading-1 flex h-full w-full items-center justify-center bg-blue-300 text-[15px] font-medium'
-                    delayMs={600}
-                >
-                   {message.role === 'user' ? 'M' : 'AI'}
-                </Avatar.Fallback>
-            </Avatar.Root>
-            <div className={
-                clsx(
-                    'rounded-xl antialiased text-sm mx-4 px-4 py-3 max-w-[75%]',
+        <div>
+            <div 
+                className={clsx(
+                    'flex items-start p-2 max-w-full',
                     {
-                        'bg-gray-100': message.role !== 'user',
-                        'text-black': message.role !== 'user',
-                        'bg-blue-500': message.role === 'user',
-                        'text-white': message.role === 'user',
+                        'flex-row-reverse': isUser
                     }
-                )
-            }>
-                <MemoizedReactMarkdown 
-                    className='prose break-words dark:prose-invert prose-p:leading-relaxed prose-pre:p-0'
-                    remarkPlugins={[remarkGfm, remarkMath]}
-                    components={{
-                        a({children}) {
-                            const urlValue = children?.toString();
-                            if(urlValue?.startsWith('http') || urlValue?.startsWith('www')){
-                                setUrl(new URL(urlValue));
-                            }
-                            return <Link href={url || '#'} target='_blank' className='text-blue-600 hover:underline active:text-blue-700 visited:text-blue-800'>
-                                        {children}
-                                    </Link>
+                )}
+            >
+                <Avatar.Root className='h-8 w-8 my-1 select-none items-center justify-center overflow-hidden rounded-full align-middle'>
+                    <Avatar.Image className='h-full w-full rounded-[inherit] object-cover' src={avatar} />
+                    <Avatar.Fallback 
+                        className='text-white leading-1 flex h-full w-full items-center justify-center bg-blue-300 text-[15px] font-medium'
+                        delayMs={600}
+                    >
+                    {isUser ? 'M' : 'AI'}
+                    </Avatar.Fallback>
+                </Avatar.Root>
+                <div className={
+                    clsx(
+                        'rounded-xl antialiased text-sm mx-4 px-4 py-3 max-w-[75%]',
+                        {
+                            'bg-gray-100': !isUser,
+                            'text-black': !isUser,
+                            'bg-blue-500': isUser,
+                            'text-white': isUser,
                         }
-                    }}
-                >
-                    {message.content}
-                </MemoizedReactMarkdown>
+                    )
+                }>
+                    <MemoizedReactMarkdown 
+                        className='prose break-words dark:prose-invert prose-p:leading-relaxed prose-pre:p-0'
+                        remarkPlugins={[remarkGfm, remarkMath]}
+                        components={{
+                            a({children}) {
+                                const urlValue = children?.toString();
+                                if(urlValue?.startsWith('http') || urlValue?.startsWith('www')){
+                                    setUrl(new URL(urlValue));
+                                }
+                                return <Link href={url || '#'} target='_blank' className='text-blue-600 hover:underline active:text-blue-700 visited:text-blue-800'>
+                                            {children}
+                                        </Link>
+                            }
+                        }}
+                    >
+                        {message.content}
+                    </MemoizedReactMarkdown>
+                </div>
             </div>
+            {
+                !isUser && !isLoading ?
+                    <div className='ml-16'>
+                        <button className='bg-transparent'>
+                            <CopyIcon className='w-4 h-4 mx-auto text-gray-500'/>
+                        </button>
+                    </div> : ''
+            }
         </div>
     );
 }
