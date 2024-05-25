e267bdd5bf1c628b13a109a03ab878c657d587d5
mich31 <michelaxel@hotmail.com>
2024-05-25 12:49:23 +0200

    add seed script

---
+++ src/scripts/seed.ts
@@ -0,0 +1,19 @@
+const { sql } = require('@vercel/postgres');
+
+async function main() {
+    await sql`
+        CREATE TABLE IF NOT EXISTS conversations (
+            id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+            username VARCHAR(255) NOT NULL,
+            email TEXT NOT NULL,
+            content TEXT NOT NULL,
+            created_at TIMESTAMP DEFAULT now(),
+            updated_at TIMESTAMP DEFAULT now()
+        );
+    `;
+    console.log(`Created "conversations" table`);
+}
+  
+main().catch((err) => {
+    console.error('An error occurred while attempting to seed the database:', err);
+});
